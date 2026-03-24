b277d71a19cc09a75563bdf9a997951ea26b4990
nicofercavv-dev <nicoleferreira281@gmail.com>
2026-03-24 19:58:38 -0300

    feat: Cria styled do container do Header

---
+++ src/presentation/components/header/Header.styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const HeaderStyled = styled.div`
+  background-image: linear-gradient(#957DCD, #523D7F);
+  height: 4.5rem;
+  width: 100%;
+  margin: 1.375rem 1.75rem;
+  border-radius: 0.9375rem;
+  opacity: 0.5;
+`;
---
+++ src/presentation/components/header/Header.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { HeaderStyled } from "./Header.styles";
+
+const Header: React.FC = () => {
+  return <HeaderStyled><p>AAAA</p></HeaderStyled>;
+};
+
+export default Header;
